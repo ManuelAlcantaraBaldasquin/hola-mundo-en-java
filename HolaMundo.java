b8c84108369bbab069ce58c46a0b61aa3d7f6272
Manuel Alcántara Baldasquín <manuel96ab@gmail.com>
2016-10-05 09:00:02 +0200

    Coloreado Hola Mundo.

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,6 +5,10 @@
 */
 public class HolaMundo { // Clase principal
   public static void main(String[] args) {
-    System.out.println("hola Mundo");
+
+    String rojo = "\033[31m";
+    String verde = "\033[32m";
+
+    System.out.println("hola" + rojo + "Mundo" + verde);
   }
 }
